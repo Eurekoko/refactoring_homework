5dab01ed502811195ba1901399988077972d86d0
Eurekoko <niuzhihong321@gmail.com>
2025-11-16 15:32:49 -0500

    Task 1.1

--- src/main/java/theater/Play.java
+++ src/main/java/theater/Play.java
@@ -1,12 +1,30 @@
 package theater;
 
+/**
+ * Represent a play.
+ */
 public class Play {
 
-    public String name;
-    public String type;
+    // I need add two methods to use these two private variable
+    private String name;
+    private String type;
 
+    /**
+     * Constructs a play.
+     *
+     * @param name the name of the play, must not be null
+     * @param type the type of the play, must not be null
+     */
     public Play(String name, String type) {
         this.name = name;
         this.type = type;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getType() {
+        return this.type;
     }
 }
